5af0948132e84b4431dcf02f3605fc3f8366533d
Nimorich <e-urezu@mail.ru>
2019-07-13 20:29:10 +0700

    Kod po knige

--- C++/1.cpp
+++ C++/1.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
-#include <cstdlib>
 using namespace std;
 
 int main()
 {
-	cout << rand();
+	int year;
+	cout << "Введите год: ";
+	cin >> year;
+	
+	if (year % 4 == 0)
+	{
+		if (year % 100 == 0)
+		{
+			if (year % 400 == 0)
+				cout << year << " - год високостный";
+			else
+				cout << year << " - год невисокостный";
+		}
+		else
+			cout << year << " - год високостный";
+	}
+	else
+		cout << year << " - год невисокостный";
+	cout << endl;
+
+	system("pause");
 	return 0;
 }
